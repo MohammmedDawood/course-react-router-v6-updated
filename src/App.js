76db17ad3936f07cf52b0e5ffc89bfd9435fb2a6
Mohammed Dawood <mahmedtony73@gmail.com>
2023-04-15 01:35:19 +0200

    createRoutesFromElements approach

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/products",
-    element: <ProductsPage />,
-  },
-]);
+const routesDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage />} />
+    <Route path="products" element={<ProductsPage />} />
+  </Route>
+);
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <HomePage />,
+//   },
+//   {
+//     path: "/products",
+//     element: <ProductsPage />,
+//   },
+// ]);
+
+const router = createBrowserRouter(routesDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
