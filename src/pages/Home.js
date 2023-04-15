c7cd943301d0d75a81ccb841c0190d41e2e1ec4c
Mohammed Dawood <mahmedtony73@gmail.com>
2023-04-15 02:37:01 +0200

    Understanding Relative & Absolute Paths

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,14 +4,14 @@ function HomePage() {
   const navigate = useNavigate();
 
   function navigationHandler() {
-    navigate("/products");
+    navigate("products");
   }
 
   return (
     <>
       <h1>HomePage</h1>
       <p>
-        Go to <Link to="/products">Products</Link>
+        Go to <Link to="products">Products</Link>
       </p>
       <p>
         <button onClick={navigationHandler}>Navigate to Products</button>
